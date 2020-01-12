4e24aa75e9e271f49f8085231a4f9005160b9e2f
Jeff Kowalczyk <kowalczj@oregonstate.edu>
2020-01-12 14:20:31 -0800

    testUtility new file to refactor game setup common functions
    Refactor the code that creates the game data by moving code here.
    Creating functions that initialization dominion gameplay lists
    along with any other opportunities for making common functions.
    Eliminate redundant game data setup code.

---
+++ projects/kowalczj/dominion/testUtility.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jan 12 14:19:50 2020
+
+@author: kowalczj
+
+Refactor the code that creates the game data by moving code here.
+Creating functions that initialization dominion gameplay lists
+along with any other opportunities for making common functions.
+Eliminate redundant game data setup code.
+"""
