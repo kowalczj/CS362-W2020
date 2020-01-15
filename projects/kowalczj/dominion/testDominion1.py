b1e91c7d0f1e6ea87a1ccd4693483c76f00fd461
Jeff Kowalczyk <kowalczj@oregonstate.edu>
2020-01-14 21:02:26 -0800

    testUtility refactor mv play function from testDominion1 testDominon2
    Move the play function from testDominion1 testDominion2
    Mechanical movement with large diff.
    
    Program does not run during this step of transitional incremental refactor.

--- projects/kowalczj/dominion/testDominion1.py
+++ projects/kowalczj/dominion/testDominion1.py
@@ -107,39 +107,3 @@ for name in player_names:
         players.append(Dominion.TablePlayer(name[1:]))
     else:
         players.append(Dominion.Player(name))
-
-# Play the game
-turn = 0
-while not Dominion.gameover(supply):
-    turn += 1
-    print("\r")
-    for value in supply_order:
-        print(value)
-        for stack in supply_order[value]:
-            if stack in supply:
-                print(stack, len(supply[stack]))
-    print("\r")
-    for player in players:
-        print(player.name, player.calcpoints())
-    print("\rStart of turn " + str(turn))
-    for player in players:
-        if not Dominion.gameover(supply):
-            print("\r")
-            player.turn(players, supply, trash)
-
-
-# Final score
-dcs = Dominion.cardsummaries(players)
-vp = dcs.loc["VICTORY POINTS"]
-vpmax = vp.max()
-winners = []
-for i in vp.index:
-    if vp.loc[i] == vpmax:
-        winners.append(i)
-if len(winners) > 1:
-    winstring = " and ".join(winners) + " win!"
-else:
-    winstring = " ".join([winners[0], "wins!"])
-
-print("\nGAME OVER!!!\n" + winstring + "\n")
-print(dcs)
--- projects/kowalczj/dominion/testUtility.py
+++ projects/kowalczj/dominion/testUtility.py
@@ -9,3 +9,50 @@ Creating functions that initialization dominion gameplay lists
 along with any other opportunities for making common functions.
 Eliminate redundant game data setup code.
 """
+
+import Dominion
+
+
+def play(supply, supply_order, players, trash):
+    """ Function to play the game
+        The data interface to the game uses:
+        - supply
+        - supply_order
+        - players
+        - trash
+        Parameterization is desired in future refactoring
+    """
+    # Play the game
+    turn = 0
+    while not Dominion.gameover(supply):
+        turn += 1
+        print("\r")
+        for value in supply_order:
+            print(value)
+            for stack in supply_order[value]:
+                if stack in supply:
+                    print(stack, len(supply[stack]))
+        print("\r")
+        for player in players:
+            print(player.name, player.calcpoints())
+        print("\rStart of turn " + str(turn))
+        for player in players:
+            if not Dominion.gameover(supply):
+                print("\r")
+                player.turn(players, supply, trash)
+
+    # Final score
+    dcs = Dominion.cardsummaries(players)
+    vp = dcs.loc["VICTORY POINTS"]
+    vpmax = vp.max()
+    winners = []
+    for i in vp.index:
+        if vp.loc[i] == vpmax:
+            winners.append(i)
+    if len(winners) > 1:
+        winstring = " and ".join(winners) + " win!"
+    else:
+        winstring = " ".join([winners[0], "wins!"])
+
+    print("\nGAME OVER!!!\n" + winstring + "\n")
+    print(dcs)
